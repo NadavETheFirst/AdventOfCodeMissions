e382532b38fa305f1dd4291a4b7e06191d488bba
NadavETheFirst <nadav.elsberg1@gmail.com>
2023-12-31 17:05:25 +0200

    first try for getValueFrom function
    tryin to write a function that takes a sting and returns a value by the defined mission

---
+++ Trebuchet/InputProcessing.py
@@ -0,0 +1,30 @@
+# This code need to take the inputs.txt and for each line take the first digit and the last digit as a 2 digit numbers and sum all of them.
+# exp.:
+# 1abc2
+# pqr3stu8vwx
+# a1b2c3d4e5f
+# treb7uchet
+# In this example, the calibration values of these four lines are 12, 38, 15, and 77. Adding these together produces 142.
+
+with open('inputs.txt','r') as inputs:
+    lines = inputs.read().split("\n")
+
+
+i = 0
+for line in lines:
+    i += 1
+    print(f"Line number {i} is: {line}")
+
+
+def getValueFrom(LineInput):
+    firstDig = 0
+    for chars in LineInput:
+        if chars.isdigit():
+            firstDig = int(chars)
+            break
+    lastDig = firstDig
+    for chars in reversed(LineInput):
+        if chars.isdigit():
+            lastDig = int(chars)
+            break
+    return (firstDig*10) +lastDig
